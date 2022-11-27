2f9b47f7e70b1306d79a1f198b433c77be1329fa
MostafaGalal1 <mostafag649.mg@gmail.com>
2022-11-27 21:23:03 +0200

    adding_features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -157,8 +157,23 @@ export class AppComponent {
   }
 
   Request(type: number){
+    var requestResult: String;
+    if (type) {
+      requestResult = this.transmitService.binaryRequest(this.first_val, this.second_val, this.bi_operator);
+    } else {
+      requestResult = this.transmitService.uniaryRequest(this.first_val, this.un_operator);
+    }
 
-    return '0';
+    this.replaceable = false;
+    this.updated = false;
+    this.precalculated = true;
+
+    if (requestResult === 'Error' || requestResult === 'Infinity' || requestResult === 'NaN') {
+      this.error = true;
+      return 'E';
+    }
+    
+    return requestResult;
   }
 
   Calculate() {
--- src/app/services/transmit.service.ts
+++ src/app/services/transmit.service.ts
@@ -4,14 +4,40 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TransmitService {
+  private url:string = "http://localhost:8080/workout/";
 
   constructor() { }
 
   uniaryRequest(first_val:number, un_operator:string){
-    return '0';
+    var un_url: string = this.url + "un_operator";
+    var un_data: any;
+    un_data = {
+        "operand": String(first_val),
+        "operator": String(un_operator)
+    };
+
+    un_url =  un_url + '?' + Object.keys(un_data).map(function (key) { return [key, un_data[key]].map(encodeURIComponent).join("="); }).join("&");
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", un_url, false);
+    xhr.send();
+
+    return xhr.responseText;
   }
 
   binaryRequest(first_val:number, second_val:number, bi_operator:string){
-   return '0';
+    var bi_url:string = this.url + "bi_operator";
+    var bi_data :any;
+    bi_data = {
+        "first_operand": String(first_val),
+        "operator": String(bi_operator),
+        "second_operand": String(second_val)
+    };
+
+    bi_url =  bi_url + '?' + Object.keys(bi_data).map(function (key) { return [key, bi_data[key]].map(encodeURIComponent).join("="); }).join("&");
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", bi_url, false);
+    xhr.send();
+
+    return xhr.responseText;
   }
 }
